0a66cfe7e989ce1f335b2dce3aba2946ffb9f5f6
Poly1581 <bpaul006@ucr.edu>
2022-10-31 15:25:42 -0700

    revamped random generation using parity, added monte carlo search to redo search results to have max frontier size instead of final frontier size. added some comments

--- main.cpp
+++ main.cpp
@@ -49,10 +49,18 @@ const vector<position> moves = {{-1,0},{1,0},{0,1},{0,-1}};
 
 //struct to hold onto search stuff
 struct node {
-	int priority = 0;
-	int depth = 0;
+	unsigned int priority = 0;
+	unsigned int depth = 0;
 	matrix state = {};
 	string path = "";
+};
+
+struct searchResult {
+	unsigned int depth = 0;
+	long unsigned int frontier = 0;
+	long unsigned int visited = 0;
+	long int time = 0;
+	string path;
 };
 
 //make a matrix of a certain size
@@ -117,18 +125,60 @@ vector<matrix> getChildren(matrix& m) {
 	return children;
 }
 
-//make a random matrix (useful for testing purposes)
-matrix randomMatrix(int size) {
-	matrix r = makeMatrix(size,"solved");
-	int moves = rand()%(1000*size*size);
-	while(moves > 0) {
-		//replace matrix r with a random child and repeat some number of times
-		vector<matrix> children = getChildren(r);
-		int index = rand()%children.size();
-		r = children.at(index);
-		moves--;
+//count the number of inversions in a matrix
+int getInversions(matrix& m) {
+	int size = m.size();
+	int inversions = 0;
+	vector<int> tiles = {};
+	//unwrap matrix to vector
+	for(int r = 0; r < size; r++) {
+		for(int c = 0; c < size; c++) {
+			tiles.push_back(m.at(r).at(c));
+		}
 	}
-	return r;
+	//loop to find inversions
+	for(int t = 0; t < tiles.size()-1; t++) {
+		if(tiles.at(t) == 0) {
+			continue;
+		}
+		int tileInversions = 0;
+		for(int t2 = t+1; t2 < tiles.size(); t2++) {
+			if(tiles.at(t2) == 0) {
+				continue;
+			}
+			if(tiles.at(t) > tiles.at(t2)) {
+				tileInversions++;
+			}
+		}
+		inversions+=tileInversions;
+	}
+	return inversions;
+}
+
+//check if matrix is valid
+bool isValid(matrix& m) {
+	return getInversions(m)%2==0;
+}
+
+//swap number of inversions from odd to even
+//find the first pair of nonempty tiles and swap them
+void changeParity(matrix& m) {
+	//if the empty space is in the top left corner, swap the two to the right of it
+	if(m.at(0).at(0) == 0) {
+		int temp = m.at(0).at(1);
+		m.at(0).at(1) = m.at(0).at(2);
+		m.at(0).at(2) = temp;
+	//if the empty space is at the second space in the first row, swap the first and the third
+	} else if(m.at(0).at(1) == 0) {
+		int temp = m.at(0).at(0);
+		m.at(0).at(0) = m.at(0).at(2);
+		m.at(0).at(2) = temp;
+	//if the empty space is not at the first or second space, swap them.
+	} else {
+		int temp = m.at(0).at(0);
+		m.at(0).at(0) = m.at(0).at(1);
+		m.at(0).at(1) = temp;
+	}
 }
 
 //get the id of a matrix by joining tiles in a row with "|" and rows with newline
@@ -148,6 +198,30 @@ string id(matrix& m) {
 	}
 	id+="\n";
 	return id;
+}
+
+//make a random matrix (useful for testing purposes)
+matrix randomMatrix(int size) {
+	vector<int> tiles;
+	for(int t = 0; t < size*size; t++) {
+		tiles.push_back(t);
+	}
+	matrix m = {};
+	//select random tiles in a valid range and insert them into the matrix
+	for(int r = 0; r < size; r++) {
+		vector<int> row = {};
+		for(int c = 0; c < size; c++) {
+			int randIndex = rand()%(tiles.size());
+			row.push_back(tiles.at(randIndex));
+			tiles.erase(tiles.begin()+randIndex);
+		}
+		m.push_back(row);
+	}
+	//if we have made a game that is unsolvable, fix it
+	if(!isValid(m)) {
+		changeParity(m);
+	}
+	return m;
 }
 
 //uniform cost heuristic
@@ -192,7 +266,7 @@ int manhattanDistance(matrix& current, matrix& goal) {
 
 //A* implementation
 //takes an initial matrix, goal matrix, and a heuristic function pointer
-void generalSearch(matrix& initial, matrix& goal, int (*heuristic) (matrix&, matrix&)) {
+searchResult generalSearch(matrix& initial, matrix& goal, int (*heuristic) (matrix&, matrix&)) {
 	milliseconds start = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 
 	//comparison function to use priority queue
@@ -205,7 +279,7 @@ void generalSearch(matrix& initial, matrix& goal, int (*heuristic) (matrix&, mat
 	priority_queue<node, vector<node>, decltype(compare)> nodes(compare);
 	nodes.push({0,0,initial,id(initial)});
 
-	int maxFrontier = nodes.size();
+	long unsigned int maxFrontier = nodes.size();
 
 	while(!nodes.empty()) {
 		node currNode = nodes.top();
@@ -215,24 +289,19 @@ void generalSearch(matrix& initial, matrix& goal, int (*heuristic) (matrix&, mat
 		if(nodes.size() > maxFrontier) {
 			maxFrontier = nodes.size();
 		}
-		cout << "Best state to expand with depth (g(n)) " << currNode.depth << " and heuristic (h(n)) " << currNode.priority-currNode.depth << endl;
-		cout << id(currNode.state);
+		// cout << "Best state to expand with depth (g(n)) " << currNode.depth << " and heuristic (h(n)) " << currNode.priority-currNode.depth << endl;
+		// cout << id(currNode.state);
 		//if we have found the goal state, print the search result (I know I could return the values but I dont wan't to have to deal with nonhomogeneous data types)
 		if(currNode.state == goal) {
 			milliseconds end = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
-			cout << currNode.path << endl;
-			cout << "Solution found at depth " << currNode.depth << endl;
-			cout << "Visited " << visited.size() << " nodes" << endl;
-			cout << maxFrontier << " in frontier"<< endl;
-			cout << "Search took " << (end.count()-start.count()) << " milliseconds" << endl;
-			return;
+			return {currNode.depth, maxFrontier, visited.size(), (end.count()-start.count()), currNode.path};
 		}
 		vector<matrix> children = getChildren(currNode.state);
 		for(matrix child:children) {
 			//check if we have visited the child previously
 			if(visited.find(id(child)) == visited.end()) {
-				int depth = currNode.depth+1;
-				int priority = depth+heuristic(child,goal);
+				unsigned int depth = currNode.depth+1;
+				unsigned int priority = depth+heuristic(child,goal);
 				string path = currNode.path+id(child);
 				//insert the node into the queue but do not mark as visited
 				nodes.push({priority,depth,child,path});
@@ -240,6 +309,110 @@ void generalSearch(matrix& initial, matrix& goal, int (*heuristic) (matrix&, mat
 		}
 	}
 	cout << "NO SOLUTION FOUND" << endl;
+	return {0,0,0,0};
+}
+
+// struct searchResult {
+// 	unsigned int depth = 0;
+// 	long unsigned int frontier = 0;
+// 	long unsigned int visited = 0;
+// 	long int time = 0;
+// 	string path;
+// };
+
+
+
+//monte carlo simulation
+void monteCarlo(int size, int simulations, int (*heuristic) (matrix&, matrix&)) {
+	long int totalTime = 0;
+	//keep track of the number of the total number of states per depth, the nubmer of frontier nodes per depth, and the number of visited nodes per depth
+	vector<unsigned int> numDepth = {};
+	vector<long unsigned > numFrontier = {};
+	vector<long unsigned int> numVisited = {};
+	vector<long int> numTime = {};
+	matrix goal = makeMatrix(size, "solved");
+	for(int sim = 0; sim < simulations; sim++) {
+		//progress indication
+		if(sim%100 == 0 && sim != 0) {
+			cout << "Completed " << sim << " simulations"  << endl;
+			cout << "Average time per solution " << (double)totalTime/(double)sim << " milliseconds" << endl;
+		}
+		//make a random matrix and solve it
+		matrix initial = randomMatrix(size);
+		searchResult solution = generalSearch(initial, goal, heuristic);
+		//expand the count vectors
+		while(numDepth.size() <= solution.depth) {
+			numDepth.push_back(0);
+			numFrontier.push_back(0);
+			numVisited.push_back(0);
+			numTime.push_back(0);
+		}
+		//add data from solution to vectors
+		numDepth.at(solution.depth)++;
+		numFrontier.at(solution.depth)+=solution.frontier;
+		numVisited.at(solution.depth)+=solution.visited;
+		numTime.at(solution.depth)+=solution.time;
+		totalTime+=solution.time;
+	}
+	vector<double> averageFrontier = {};
+	vector<double> averageVisited = {};
+	vector<double> averageTime = {};
+	//take average of each vector
+	for(int depth = 0; depth < numDepth.size(); depth++) {
+		while(averageFrontier.size() <= depth) {
+			averageFrontier.push_back(0);
+			averageVisited.push_back(0);
+			averageTime.push_back(0);
+		}
+		averageFrontier.at(depth) = (double)numFrontier.at(depth)/(double)numDepth.at(depth);
+		averageVisited.at(depth) = (double)numVisited.at(depth)/(double)numDepth.at(depth);
+		averageTime.at(depth) = (double)numTime.at(depth)/(double)numDepth.at(depth);
+	}
+	//print outcomes
+	cout << "DEPTH:" << endl;
+	cout << "[";
+	for(int d = 0; d < numDepth.size(); d++) {
+		if(numDepth.at(d) != 0) {
+			cout << d;
+			if(d != numDepth.size()-1) {
+				cout << ", ";
+			}
+		}
+	}
+	cout << "]" << endl;
+	cout << "VISITED:" << endl;
+	cout << "[";
+	for(int d = 0; d < numDepth.size(); d++) {
+		if(numDepth.at(d) != 0) {
+			cout  << averageVisited.at(d);
+			if(d != numDepth.size()-1) {
+				cout << ", ";
+			}
+		}
+	}
+	cout << "]" << endl;
+	cout << "FRONTIER:" << endl;
+	cout << "[";
+	for(int d = 0; d < numDepth.size(); d++) {
+		if(numDepth.at(d) != 0) {
+			cout << averageFrontier.at(d);
+			if(d != numDepth.size()-1) {
+				cout << ", ";
+			}
+		}
+	}
+	cout << "]" << endl;
+	cout << "TIME:" << endl;
+	cout << "[";
+	for(int d = 0; d < numDepth.size(); d++) {
+		if(numDepth.at(d) != 0) {
+			cout << averageTime.at(d);
+			if(d != numDepth.size()-1) {
+				cout << ", ";
+			}
+		}
+	}
+	cout << "]" << endl;
 }
 
 //driver function
@@ -272,11 +445,19 @@ int main() {
 		return -1;
 	}
 
+	searchResult solution;
 	if(heuristic == 1) {
-		generalSearch(initial, goal, &uniformCost);
+		solution = generalSearch(initial, goal, &uniformCost);
 	} else if(heuristic == 2) {
-		generalSearch(initial, goal, &misplacedTile);
+		solution = generalSearch(initial, goal, &misplacedTile);
 	} else if(heuristic == 3) {
-		generalSearch(initial, goal, &manhattanDistance);
+		solution = generalSearch(initial, goal, &manhattanDistance);
 	}
+	cout << "Solution found" << endl;
+	cout << "Path" << endl;
+	cout << solution.path;
+	cout << "Depth of solution " << solution.depth << endl;
+	cout << "Number of visited nodes " << solution.visited << endl;
+	cout << "Max frontier size " << solution.frontier << endl;
+	cout << "Search took " << solution.time << " milliseconds" << endl;
 }
